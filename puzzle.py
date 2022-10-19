bf12734d8a22166507875bd6de51d0dd9c9558be
Dynara <outdoorsy02@gmail.com>
2022-10-19 09:35:27 -0600

    puzzle: working on encapsulation

--- puzzle.py
+++ puzzle.py
@@ -12,37 +12,45 @@ from jumper import Jumper
 
 class Puzzle:
 
+    def __init__(self):
+        
+        self._jumper = Jumper()
+        self._player_set_of_guesses = set()
+        self._word = Wordslist()
+        self._lives = 4
+
     def compare_letter_to_word(self):
 
         # Bring in class Wordslist with the rand_words method
-        word = Wordslist.rand_words(self)
-        # making the word a iterable set
+        word = self._word.rand_words(self)
+        # making the word a iterable set  
         word_letters = set(word) 
         # Set to uppercase and separate so letters can be accessed individually
         alphabet = set(string.ascii_uppercase)
+
         # Empty set that will be filled with player guesses
-        player_set_of_guesses = set()  
+        #player_set_of_guesses = set()  
 
         # Starting number of lives
-        lives = 4
+        # lives = 4
             
         # For continuation of play
-        while len(word_letters) > 0 and lives > 0:
+        while len(word_letters) > 0 and self._lives > 0:
             # Letters used
-            print('You have ', lives ,'more parachute strings and you have used these letters: ', ' '.join(player_set_of_guesses))
+            print('You have ', self._lives ,'more parachute strings and you have used these letters: ', ' '.join(self._player_set_of_guesses))
 
         # For current word
-        word_list = [letter if letter in player_set_of_guesses else '-' for letter in word]
+        word_list = [letter if letter in self._player_set_of_guesses else '-' for letter in word]
         # From Jumper class, print visual with number of lives left
-        print(Jumper.jumper_visual_dict[lives])
+        print(self._jumper.jumper_visual_dict[lives])
         print('Current word: ', ' '.join(word_list))
 
         # **Input, not sure if this will be in this class
         player_guess = input('Guess a letter: ').upper()
 
-        if player_guess in alphabet - player_set_of_guesses:
+        if player_guess in alphabet - self._player_set_of_guesses:
             # Add player guess to set
-            player_set_of_guesses.add(player_guess)
+            self._player_set_of_guesses.add(player_guess)
             # Check if letter guessed in word
             if player_guess in word_letters:
                 # Remove letter from the iterable set since it has been guessed
@@ -55,7 +63,7 @@ class Puzzle:
                 print(f'\n{player_guess} is not in the word.')
 
         # Check if player_guess is already in the set of letters guessed
-        elif player_guess in player_set_of_guesses:
+        elif player_guess in self._player_set_of_guesses:
             print(f'\n{player_guess} has been used. Guess another letter.')
         
         # If player enters something besides a single letter
@@ -65,7 +73,7 @@ class Puzzle:
         # Check if there are any remaining lives, if not then end game.
         if lives == 0:
             # Show jumper with X for head
-            print(Jumper.jumper_visual_dict[lives])
+            print(self._jumper.jumper_visual_dict[lives])
             # Reveal word 
             print(f'Sorry, your parachute failed. The word was {word}.')
         else:
