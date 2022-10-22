b50e9f59ed8f2576d582bd1ad7646f84d928ff84
TeigenB <wea20004@byui.edu>
2022-10-21 20:16:51 -0600

    added comments to my portion of the code

--- game/jumper.py
+++ game/jumper.py
@@ -1,18 +1,29 @@
-"""-Jumper (Teigen)
+
+"""Outline Information from Week 5:
+    -Jumper (Teigen)
     For each life have associated drawing
     When tries reaches 4 guesses, game over
     Head turns to x when game over
     print diagram from an if statement"""
 
-#from game.puzzle import Puzzle
 
 class Jumper:
+    #The job of the class is to provide visuals and print them in the terminal based on the 
+    #number of lives the user has left. 
+
+    #Attributes: just the lives count, which is brought in as a parameter rather than imported
+    #Relationships: This class doesn't import anything, but is called on by the Puzzle class
 
     def _init_(self):
-        self._lives = self._puzzle.player_input
+        #because I'm not importing other classes I don't need to initialize any variables. 
+        #I just did the initialization to fit with the class syntax
+        pass
         
-
+        
     def lines(self, lives):
+    #This method asks for two parameters, but when calling it you only need to include one in
+    #the parentheses. The nature of self means that the method will grab the instance of self
+    #just because I put it as a requirement in the parameters spot. Very interesting.
         if lives == 4:
             print("          \n"
                   "   ____   \n"
