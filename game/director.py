2cf37d46fc2b92413cd8e6cf47364d8aac5f4b9a
jmadondo1722 <jmadondo1722@gmail.com>
2022-10-22 06:12:48 +0200

    Removed the unused code and added comments in the Director class

--- game/director.py
+++ game/director.py
@@ -1,34 +1,38 @@
 from game.puzzle import Puzzle
-#from game.jumper import Jumper
-
 
 
 class Director:
-    def __init__(self):
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
 
+    Attributes:
+        jumper (Jumper): The game's jumper.
+        is_playing (boolean): Whether or not to keep playing.
+        puzzle (Puzzle): The game's puzzle.
+        words_list: For generating random words to be used in the puzzle.
+    """
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
         self._puzzle = Puzzle()
         self._is_playing = True
-        #self._jumper = Jumper()
-        # self._terminal_service = TerminalService()
-        # self._lives = None
+        
     def start_game(self):
 
         while self._is_playing:
             self._get_inputs()
-         #   self._do_updates()
             self._do_outputs()
             self._end_game()
 
     def _get_inputs(self):
-        # self._puzzle.player_input()
+
         self._puzzle.compare_letter_to_word()
 
-   # def _do_updates(self):
-    #    self._puzzle.lives_left()
-        
-
     def _do_outputs(self):
-        #self._jumper.lines(self._lives)
         
         # Will continue to run until game over
         self._puzzle.lives_left() 
